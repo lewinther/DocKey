7a3a6237bc511152327a495351d7ad324905a4b3
mealora <alme22ab@student.cbs.dk>
2023-11-06 17:48:45 +0100

    added dock filter and styles

---
+++ src/components/DockFilter.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+const DockFilter = ({ onDockSelect, dockNumbers }) => {
+  const [dockNumber, setDockNumber] = useState('');
+
+  const handleSelectionChange = (selectedDockNumber) => {
+    onDockSelect(selectedDockNumber);
+  };
+
+  const handleChange = (e) => {
+    const newDockNumber = e.target.value;
+    setDockNumber(newDockNumber);
+    handleSelectionChange(newDockNumber);
+  };
+
+  return (
+    <form className="dock-select-form" onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="dock-select" className="dock-select-label">
+        <select
+          id="dock-select"
+          value={dockNumber}
+          onChange={handleChange}
+          className="dock-select"
+        >
+          <option value="">Choose dock number</option>
+          {dockNumbers.map((number) => (
+            <option key={number} value={number}>
+              {number}
+            </option>
+          ))}
+        </select>
+      </label>
+    </form>
+  );
+};
+
+export default DockFilter;
