0917914299b33ad580a5bdf97f909a6f0a576d07
lewinther <125894035+lewinther@users.noreply.github.com>
2023-11-06 13:36:13 +0100

    Import of searchbar component

--- src/pages/MyInbox.js
+++ src/pages/MyInbox.js
@@ -3,6 +3,9 @@ import { Fragment } from "react";
 // CSS import
 import "../../src/styles.css";
 
+//Components import
+import SearchBar from './components/SearchBar';
+
 export default function MyInbox() {
     return(
     <Fragment>
