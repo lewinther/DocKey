5b0e84be856e24765d79dc21cab565a667076998
mealora <alme22ab@student.cbs.dk>
2023-11-07 21:02:04 +0100

    added buttons and some logic in newmessage

--- src/components/NewMessageCardContainer.js
+++ src/components/NewMessageCardContainer.js
@@ -2,12 +2,17 @@ import { Fragment } from "react";
 
 import InputMessage from "./InputMessage";
 
-export default function NewMessageCardContainer(){
+export default function NewMessageCardContainer({ messageContent, onContentChange }) {
+    //takes current value of text area and updates parent//
+    const handleMessageChange = (event) => {
+        onContentChange(event.target.value);
+    };
 
     return(
+        //handle message change calls oncontentchange and updates parent//
         <Fragment>
             <section className="new-message-container in-column">
-                <InputMessage />
+                <textarea onChange={handleMessageChange} value={messageContent} />
             </section>
         </Fragment>
     )
--- src/pages/NewMessage.js
+++ src/pages/NewMessage.js
@@ -1,4 +1,5 @@
 import { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 // CSS import
 import "../../src/styles.css";
@@ -10,18 +11,46 @@ import DockFilter from "../components/DockFilter";
 
 export default function NewMessage() {
     const dockNumbers = ['D1', 'D2', 'D3', 'D4'];
+    //will store dock number and message text//
+    const [selectedDock, setSelectedDock] = useState('');
+    const [messageContent, setMessageContent] = useState('');
   
+    //update selected dock state when user selects a dock//
     const handleDockSelection = (selectedDockNumber) => {
     console.log(`Selected dock number: ${selectedDockNumber}`);
+    setSelectedDock(selectedDockNumber);
   };
 
-    return(
+    const handleMessageContentChange = (content) => {
+        setMessageContent(content);
+    };
+
+    //actions when send button clicked - ensure both dock no and message content exist//
+    const handleSendMessage = () => {
+        if (!selectedDock || !messageContent) {
+            alert('Ensure a dock number has been selected');
+            return;
+        }
+        //reset form after sending//
+        setMessageContent('');
+        alert(`Message sent to ${selectedDock}!`);
+    };
+
+    return (
         <Fragment>
             <h1>New Message</h1>
-            <h3>Which dock do you want to contact ?</h3>
+            <h3>Which dock do you want to contact?</h3>
             <DockFilter onDockSelect={handleDockSelection} dockNumbers={dockNumbers} />
             <h3>Your Message:</h3>
-            <NewMessageCardContainer />
+            <NewMessageCardContainer 
+                messageContent={messageContent}
+                onContentChange={handleMessageContentChange} />
+            <div className="button-container">
+                <div className="button-group">
+                    <Link className="WhiteButton link" to={`/AttachPhoto`}>Attach Photo</Link>
+                    <button className="BlueButton" onClick={handleSendMessage}>Send</button>
+                </div>
+            </div>
         </Fragment>
-    )
+    );
 }
