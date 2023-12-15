5f6d18c398d298e24d1d81fbf42f9c39af44d7a5
Reese181 <alri22ac@student.cbs.dk>
2023-12-15 20:05:29 +0100

    fixed css + chatlistcard

--- src/components/ChatListCard.js
+++ src/components/ChatListCard.js
@@ -3,6 +3,7 @@ import { Fragment } from 'react';
 export default function ChatCard({
   chatPartnerID,
   chatPartnerUsername,
+  chatPartnerImg,
   chatDate,
   chatPreviewText,
   chatTextStyle,
@@ -12,16 +13,39 @@ export default function ChatCard({
     <Fragment>
       <div className="message-card-container" id={`chat-${chatPartnerID}`} onClick={onClick}>
         {/* The rest of your ChatCard contents */}
-        <div className="card">
+        <div>
           <div className="in-line">
             <section className="message-card-body">
               <div className="in-line">
-                <p className="bold" id={`newsTitle username-${chatPartnerID}`}>{chatPartnerUsername}</p>
-                <h5 id={`newsDate date-${chatPartnerID}`}>{chatDate}</h5>
+              {chatPartnerImg && (
+                <div className='profile-img-container'>
+                  <div className='square'>
+                  <img
+                    className="profile-img-container"
+                    src={chatPartnerImg}
+                    alt={chatPartnerUsername + " image"}
+                  />
+                  </div>
+                </div>
+              )}
+              {!chatPartnerImg && (
+                <div className='profile-img-container'>
+                  <div className='square'>
+                    <div className='profile-img-alt-font'>{chatPartnerUsername}</div>
+                  </div>
+                </div>
+              )}
+              <div className= "width-75">
+                <div className="in-line chatlist-card-title">
+                  <div className="chatlist-card-username bold" id={`username-${chatPartnerID}`}>{chatPartnerUsername}</div>
+                  <div className="chatlist-card-date" id={`newsDate date-${chatPartnerID}`}>{chatDate}</div>
+                </div>
+                <div className="meta-text">
+                  <div className={`chatlist-card-preview ${chatTextStyle}`}>{chatPreviewText}</div>
+                </div>
               </div>
-              <div className="meta-text">
-                <p className={`${chatTextStyle}`}>{chatPreviewText}</p>
               </div>
+
             </section>
           </div>
         </div>
