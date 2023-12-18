79b9456c2c2c637acb03094522bdcd77ab027ee7
lewinther <125894035+lewinther@users.noreply.github.com>
2023-12-18 23:42:55 +0100

    Changed formatting for Profile.js

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -13,71 +13,85 @@ export default function UserInfo({ profileImage, fullName, phoneNr, eMail }) {
 
   function handleAttachClick() {
     fileInputRef.current.click();
-  };
-
+  }
 
   return (
     <Fragment>
-      <div className="user-container">
-        <div className="user-image-container">
-          {profileImage && (
-            <div className="user-image">
-              <img className="news-card-img" src={profileImage} />
-            </div>
-          )}
-          {!profileImage && (
-            <div className="user-image-empty">
-              <img src={defaultProfileImage} style={{ width: "100%" }} />
-            </div>
+      <div className="user-container-container">
+        <div className="user-container">
+          <div className="user-image-container">
+            {profileImage && (
+              <div className="user-image">
+                <img className="news-card-img" src={profileImage} />
+              </div>
+            )}
+            {!profileImage && (
+              <div className="user-image-empty">
+                <img src={defaultProfileImage} style={{ width: "100%" }} />
+              </div>
+            )}
+          </div>
+          <div className="user-info">
+            <p>
+              Name: <br /> <b>{fullName}</b>
+            </p>
+            <p>
+              Phone: <br /> <b>{phoneNr}</b>
+            </p>
+            <p>
+              E-mail: <br /> <b>{eMail}</b>
+            </p>
+          </div>
+          <img
+            onClick={editProfile}
+            src={editProfileIcon}
+            style={{ width: "8%", height: "8%" }}
+            alt="Edit profile"
+          />
+          {editProfile === true && (
+            <>
+              <div className="user-image-container">
+                {profileImage && (
+                  <>
+                    <div className="user-image">
+                      <img className="news-card-img" src={profileImage} />
+                    </div>
+                    <button
+                      className="delete-image-button"
+                      onClick={onDeleteImage}
+                      style={{ maxHeight: "6vh" }}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="attach-button"
+                      onClick={handleAttachClick}
+                    >
+                      Attach Photo
+                    </button>
+                  </>
+                )}
+                {!profileImage && (
+                  <>
+                    <div className="user-image-empty">
+                      <img
+                        src={defaultProfileImage}
+                        style={{ width: "100%" }}
+                      />
+                    </div>
+                    <button
+                      className="attach-button"
+                      onClick={handleAttachClick}
+                    >
+                      Attach Photo
+                    </button>
+                  </>
+                )}
+              </div>
+            </>
           )}
         </div>
-        <div className="user-info">
-          <p>
-            Name: <br /> <b>{fullName}</b>
-          </p>
-          <p>
-            Phone: <br /> <b>{phoneNr}</b>
-          </p>
-          <p>
-            E-mail: <br /> <b>{eMail}</b>
-          </p>
-        </div>
-        <img
-          onClick={editProfile}
-          src={editProfileIcon}
-          style={{ width: "8%", height: "8%" }}
-          alt="Edit profile"
-        />
-        {editProfile === true && (
-          <>
-            <div className="user-image-container">
-              {profileImage && (
-                <>
-                  <div className="user-image">
-                    <img className="news-card-img" src={profileImage} />
-                  </div>
-                  <button
-                    className="delete-image-button"
-                    onClick={onDeleteImage}
-                    style={{ maxHeight: "6vh" }}
-                  >
-                    Delete
-                  </button>
-                  <button className="attach-button" onClick={handleAttachClick}>
-                  Attach Photo
-                </button>
-                </>
-              )}
-              {!profileImage && (
-                <><div className="user-image-empty">
-                  <img src={defaultProfileImage} style={{ width: "100%" }} />
-                </div><button className="attach-button" onClick={handleAttachClick}>
-                    Attach Photo
-                  </button></>
-              )}
-            </div>
-          </>
-        )}
+        <p className="bold">Change password</p>
       </div>
     </Fragment>
   );
