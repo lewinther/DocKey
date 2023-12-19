ff1107cb8a33716d54132ff48eaac1b641d4681d
lewinther <125894035+lewinther@users.noreply.github.com>
2023-12-19 22:20:44 +0100

    Hide input while creating new password

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,6 +1,7 @@
 import { Fragment, useRef, useState } from "react";
 import Parse from "parse";
-import { setProfileImage } from '../parse/parseHelper';
+import { setProfileImage, setNewPassword } from "../parse/parseHelper";
+import React from "react";
 
 // stores
 import useUserStore from "../stores/UserStore";
@@ -10,9 +11,9 @@ import defaultProfileImage from "../assets/logo6.png";
 import editProfileIcon from "../assets/Icon-edit-profile.png";
 
 // Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = 'l3GQPvwNSbOEWclaYe7G7zfmdh2lQP2kHquXOGbJ';
-const PARSE_JAVASCRIPT_KEY = 'h9PTAAitCJFul7XadjhQbXFaK1N8VGZdJodYl5Tx';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_APPLICATION_ID = "l3GQPvwNSbOEWclaYe7G7zfmdh2lQP2kHquXOGbJ";
+const PARSE_JAVASCRIPT_KEY = "h9PTAAitCJFul7XadjhQbXFaK1N8VGZdJodYl5Tx";
+const PARSE_HOST_URL = "https://parseapi.back4app.com/";
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
 
@@ -22,23 +23,37 @@ export default function UserInfo({ profileImage, fullName, phoneNr, eMail }) {
   const fileInputRef = useRef(null);
   const [editProfile, setEditProfile] = useState(false);
   const [changePhoto, setChangePhoto] = useState(false);
-  const [imageFile, setImageFile] = useState(null)
+  const [changePassword, setChangePassword] = useState(false);
+  const [imageFile, setImageFile] = useState(null);
+  const [passwordText, setPasswordText] = React.useState("");
+  const [secondPasswordText, setSecondPasswordText] = React.useState("");
   let imagePreview = imageFile ? imageFile.previewUrl : null;
-
 
   function handleClickEditProfile() {
     setEditProfile(true);
-    return editProfile;
   }
 
-  function handleClickChangePhoto(){
+  function handleClickChangePhoto() {
     setChangePhoto(true);
-    return changePhoto;
   }
 
-  function handleGoBack(){
+  function handleClickChangePassword() {
+    setChangePassword(true);
+  }
+
+  function handlePassWordTextChange(content) {
+    setPasswordText(content.target.value);
+  }
+
+  function handleSecondPassWordTextChange(content) {
+    setSecondPasswordText(content.target.value);
+  }
+
+  function handleGoBack() {
     setEditProfile(false);
-    return editProfile;
+    setChangePassword(false);
+    setPasswordText("");
+    setSecondPasswordText("");
   }
 
   function handleUploadClick() {
@@ -52,25 +67,38 @@ export default function UserInfo({ profileImage, fullName, phoneNr, eMail }) {
       setImageFile({ file, previewUrl });
       event.target.value = null;
     }
-  };
+  }
 
-  function onDeleteImage(){
+  function onDeleteImage() {
     setImageFile(null);
     if (fileInputRef.current) {
       fileInputRef.current.value = null;
     }
     setChangePhoto(false);
-  };
+  }
 
-  function refresh(){
-    window.location.reload("Refresh")
+  function refresh() {
+    window.location.reload("Refresh");
   }
 
   async function handleSaveClick() {
     if (imageFile) {
       await setProfileImage(user, imageFile.file);
-        if('Profile image updated successfully!') {refresh();}
-    };
+      if ("Profile image updated successfully!") {
+        refresh();
+      }
+    }
+  }
+
+  async function handleSavePasswordClick() {
+    if (passwordText === secondPasswordText) {
+      await setNewPassword(user, passwordText);
+      if ("Password updated successfully!") {
+        refresh();
+      }
+    } else {
+    }
+    console.log("handleSavePasswordClick");
   }
 
   return (
@@ -107,80 +135,209 @@ export default function UserInfo({ profileImage, fullName, phoneNr, eMail }) {
             alt="Edit profile"
           />
         </div>
-        <p className="bold">Change password</p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            alignItems: "center",
+            paddingBottom: "2%",
+          }}
+        >
+          <p className="bold" onClick={handleClickChangePassword}>
+            Change password
+          </p>
+          {changePassword === true && (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "95%",
+                  paddingBottom: "2%",
+                }}
+              >
+                <input
+                  type={"password"}
+                  style={{
+                    height: "6vh",
+                    textAlign: "center",
+                    fontSize: "90%",
+                    width: "50%",
+                    marginLeft: "2%",
+                    borderRadius: '10px',
+                    border:'none'
+                  }}
+                  id="password"
+                  onChange={handlePassWordTextChange}
+                  value={passwordText}
+                  placeholder="Type in new password"
+                />
+                <input
+                  type={"password"}
+                  style={{
+                    height: "6vh",
+                    textAlign: "center",
+                    fontSize: "90%",
+                    width: "50%",
+                    marginLeft: "2%",
+                    borderRadius: '10px',
+                    border:'none'
+                  }}
+                  id="password"
+                  onChange={handleSecondPassWordTextChange}
+                  value={secondPasswordText}
+                  placeholder="Repeat new password"
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "95%",
+                  margin: " auto",
+                }}
+              >
+                <button
+                  className="blue-button"
+                  style={{
+                    width: "40%",
+                    height: "5vh",
+                    marginRight: "2%",
+                  }}
+                  onClick={handleSavePasswordClick}
+                >
+                  Save password
+                </button>
+                <button
+                  className="blue-button"
+                  style={{
+                    height: "5vh",
+                    width: "40%",
+                    backgroundColor: "#b3251b",
+                    borderColor: "#b3251b",
+                  }}
+                  onClick={handleGoBack}
+                >
+                  Close
+                </button>
+              </div>
+            </>
+          )}
+        </div>
       </div>
       {editProfile === true && (
         <>
-        {changePhoto === false && (
-        <>
-          <div className="in-column" style={{alignSelf:'right', alignItems: 'center'}}>
-            <button
-              className="blue-button"
-              style={{ marginBottom:'2%', width:'20vh'}}
-              onClick={handleClickChangePhoto}
-            >
-              Change photo
-            </button>
-            <button 
-              className="blue-button" 
-              style={{ height:'5vh', width:'20vh', backgroundColor:'#b3251b', borderColor:'#b3251b'}}
-              onClick={handleGoBack}
-            >
-              Go back
-            </button>
-          </div>
-        </>
-        )}
-        {changePhoto === true && (
-        <>
-          <div className="in-line" style={{marginTop:'3%', height:'11vh'}}>
-            <div style={{ width:'50%', display: 'flex', flexDirection: 'row', alignContent: 'center' }}>
-            {imagePreview && (
-                <div style={{ display: 'flex',flexDirection:'column', alignContent: 'center' }} >
-                    <img
-                      src={imagePreview}
-                      alt="Preview"
-                      style={{ display: 'block', marginLeft: 'auto', marginRight:'auto', width:'52%' }}
-                    />
+          {changePhoto === false && (
+            <>
+              <div
+                className="in-column"
+                style={{ alignSelf: "right", alignItems: "center" }}
+              >
+                <button
+                  className="blue-button"
+                  style={{ marginBottom: "2%", width: "20vh" }}
+                  onClick={handleClickChangePhoto}
+                >
+                  Change photo
+                </button>
+                <button
+                  className="blue-button"
+                  style={{
+                    height: "5vh",
+                    width: "20vh",
+                    backgroundColor: "#b3251b",
+                    borderColor: "#b3251b",
+                  }}
+                  onClick={handleGoBack}
+                >
+                  Go back
+                </button>
+              </div>
+            </>
+          )}
+          {changePhoto === true && (
+            <>
+              <div
+                className="in-line"
+                style={{ marginTop: "3%", height: "11vh" }}
+              >
+                <div
+                  style={{
+                    width: "50%",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignContent: "center",
+                  }}
+                >
+                  {imagePreview && (
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignContent: "center",
+                      }}
+                    >
+                      <img
+                        src={imagePreview}
+                        alt="Preview"
+                        style={{
+                          display: "block",
+                          marginLeft: "auto",
+                          marginRight: "auto",
+                          width: "52%",
+                        }}
+                      />
+                    </div>
+                  )}
                 </div>
-            )}
-            </div>
-            <input
-              type="file"
-              ref={fileInputRef}
-              onChange={handleImageChange}
-              accept="image/*"
-              style={{ display: "none" }}
-            />
-            <div 
-              className="in-column" 
-              style={{height:'5vh', width:'50%', alignSelf:'right', alignItems: 'center'}}
-            >
-            <button
-              className="blue-button"
-              style={{ marginBottom:'4%', width:'20vh'}}
-              onClick={handleUploadClick}
-            >
-              New photo
-            </button>
-            <button
-              className="blue-button"
-              style={{ marginBottom:'4%', width:'20vh'}}
-              onClick={handleSaveClick}
-            >
-              Save changes
-            </button>
-            <button 
-              className="blue-button" 
-              style={{ height:'5vh', width:'20vh', backgroundColor:'#b3251b', borderColor:'#b3251b'}}
-              onClick={onDeleteImage}
-            >
-              Close
-            </button>
-            </div>
-          </div>
-        </>
-        )}
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  onChange={handleImageChange}
+                  accept="image/*"
+                  style={{ display: "none" }}
+                />
+                <div
+                  className="in-column"
+                  style={{
+                    height: "5vh",
+                    width: "50%",
+                    alignSelf: "right",
+                    alignItems: "center",
+                  }}
+                >
+                  <button
+                    className="blue-button"
+                    style={{ marginBottom: "4%", width: "20vh" }}
+                    onClick={handleUploadClick}
+                  >
+                    New photo
+                  </button>
+                  <button
+                    className="blue-button"
+                    style={{ marginBottom: "4%", width: "20vh" }}
+                    onClick={handleSaveClick}
+                  >
+                    Save changes
+                  </button>
+                  <button
+                    className="blue-button"
+                    style={{
+                      height: "5vh",
+                      width: "20vh",
+                      backgroundColor: "#b3251b",
+                      borderColor: "#b3251b",
+                    }}
+                    onClick={onDeleteImage}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
         </>
       )}
     </Fragment>
--- src/parse/parseHelper.js
+++ src/parse/parseHelper.js
@@ -7,36 +7,36 @@ const PARSE_HOST_URL = "https://parseapi.back4app.com/";
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
 
-export const _sender_user_id = 'Sender_User_ID';
-export const _receiver_user_id = 'Receiver_User_ID';
-export const _message_date = 'Message_Date';
-export const _message = 'Message';
+export const _sender_user_id = "Sender_User_ID";
+export const _receiver_user_id = "Receiver_User_ID";
+export const _message_date = "Message_Date";
+export const _message = "Message";
 export const _message_text = "Message_Text";
-export const _username = 'username';
-export const _unread = 'Unread_Message';
-export const _messageId = 'objectId';
-export const _profile_image = 'profile_image';
+export const _username = "username";
+export const _unread = "Unread_Message";
+export const _messageId = "objectId";
+export const _profile_image = "profile_image";
 
 export const _messageFields = {
-    messageId: 'objectId',
-    message: 'Message',
-    date: 'Message_Date',
-    text: 'Message_Text',
-    senderId: 'Sender_User_ID',
-    receiverId: 'Receiver_User_ID',
-    unread: 'Unread_Message',
-    img : 'Image'
-}
+  messageId: "objectId",
+  message: "Message",
+  date: "Message_Date",
+  text: "Message_Text",
+  senderId: "Sender_User_ID",
+  receiverId: "Receiver_User_ID",
+  unread: "Unread_Message",
+  img: "Image",
+};
 
 export async function getUserName(userId) {
-    const userQuery = new Parse.Query(Parse.User);
-    userQuery.equalTo('objectId', userId);
-    let result = await userQuery.first();
-    return result.get(_username);
-  }
+  const userQuery = new Parse.Query(Parse.User);
+  userQuery.equalTo("objectId", userId);
+  let result = await userQuery.first();
+  return result.get(_username);
+}
 
 export async function getUnreadMessagesCount(userId, chatPartnerId) {
-  const query = new Parse.Query('Message');
+  const query = new Parse.Query("Message");
   query.equalTo(_receiver_user_id, userId);
   query.equalTo(_sender_user_id, chatPartnerId);
   query.equalTo(_unread, true);
@@ -44,21 +44,21 @@ export async function getUnreadMessagesCount(userId, chatPartnerId) {
 }
 
 export function getMessageDate(msg) {
-    const messageDate = msg.get(_message_date).toLocaleDateString();
-    return messageDate ? messageDate : 'Unknown date';
-  }
+  const messageDate = msg.get(_message_date).toLocaleDateString();
+  return messageDate ? messageDate : "Unknown date";
+}
 
 export function getMessageText(msg) {
-    return msg.get(_message_text);
-  }
+  return msg.get(_message_text);
+}
 
 export function isMessageUnread(msg) {
-    return msg.get(_unread);
-  }
+  return msg.get(_unread);
+}
 
 export function getMessageId(msg) {
-    return msg.get(_messageId);
-  }
+  return msg.get(_messageId);
+}
 
 export async function fetchDockNumbers(currentUserId) {
   const User = Parse.Object.extend("_User");
@@ -69,8 +69,8 @@ export async function fetchDockNumbers(currentUserId) {
     const dockToUserId = {};
 
     for (const user of results) {
-      const dock = user.get('dock');
-      const userId = user.id;  
+      const dock = user.get("dock");
+      const userId = user.id;
       if (dock && userId !== currentUserId) {
         docks.push(dock);
         dockToUserId[dock] = userId;
@@ -79,100 +79,116 @@ export async function fetchDockNumbers(currentUserId) {
 
     return { docks, dockToUserId };
   } catch (error) {
-    console.error('Error while fetching dock numbers', error);
+    console.error("Error while fetching dock numbers", error);
     throw error; // Re-throw the error to be handled by the caller
   }
+}
+
+export async function sendMessage(
+  receiverId,
+  senderId,
+  messageContent,
+  imageObject
+) {
+  const Message = Parse.Object.extend("Message");
+
+  // Creating pointers for sender and receiver
+  const senderPointer =
+    Parse.Object.extend("_User").createWithoutData(senderId);
+  const receiverPointer =
+    Parse.Object.extend("_User").createWithoutData(receiverId);
+
+  // Creating a new message object
+  const message = new Message();
+  message.set(_messageFields.text, messageContent);
+  message.set(_messageFields.senderId, senderPointer);
+  message.set(_messageFields.receiverId, receiverPointer);
+  message.set(_messageFields.date, new Date());
+  message.set(_messageFields.unread, true);
+
+  if (imageObject) {
+    message.set(_messageFields.img, imageObject); // Adding image pointer if image is provided
   }
 
-  export async function sendMessage(receiverId, senderId, messageContent, imageObject) {
-    const Message = Parse.Object.extend("Message");
-
-  
-    // Creating pointers for sender and receiver
-    const senderPointer = Parse.Object.extend('_User').createWithoutData(senderId);
-    const receiverPointer = Parse.Object.extend('_User').createWithoutData(receiverId);
-  
-    // Creating a new message object
-    const message = new Message();
-    message.set(_messageFields.text, messageContent);
-    message.set(_messageFields.senderId, senderPointer);
-    message.set(_messageFields.receiverId, receiverPointer);
-    message.set(_messageFields.date, new Date());
-    message.set(_messageFields.unread, true);
-  
-    if (imageObject) {
-      message.set(_messageFields.img, imageObject); // Adding image pointer if image is provided
-    }
-  
-    try {
-      await message.save();
-    } catch (error) {
-      console.error('Error while sending message:', error);
-      throw error; // Re-throw the error to be handled by the caller
-    }
-    return "message sent successfully";
+  try {
+    await message.save();
+  } catch (error) {
+    console.error("Error while sending message:", error);
+    throw error; // Re-throw the error to be handled by the caller
   }
+  return "message sent successfully";
+}
 
-  export async function uploadImage(file) {
-    const parseFile = new Parse.File(file.name, file);
-    try {
-      await parseFile.save();
-      const ImageObject = new Parse.Object("Image");
-      ImageObject.set('Image_File', parseFile);
-      await ImageObject.save();
-      return ImageObject;
-    } catch (error) {
-      console.error('Error while uploading image:', error);
-      throw error; // Re-throw the error to be handled by the caller
-    }
+export async function uploadImage(file) {
+  const parseFile = new Parse.File(file.name, file);
+  try {
+    await parseFile.save();
+    const ImageObject = new Parse.Object("Image");
+    ImageObject.set("Image_File", parseFile);
+    await ImageObject.save();
+    return ImageObject;
+  } catch (error) {
+    console.error("Error while uploading image:", error);
+    throw error; // Re-throw the error to be handled by the caller
   }
-  
-  export async function markMessageAsRead(messageId) {
-    const Message = Parse.Object.extend("Message");
-    const query = new Parse.Query(Message);
-    try {
-        const message = await query.get(messageId);
-        message.set(_unread, false);
-        await message.save();
-    } catch (error) {
-        console.error('Error while marking message as read:', error);
-        throw error;
-    }
+}
+
+export async function markMessageAsRead(messageId) {
+  const Message = Parse.Object.extend("Message");
+  const query = new Parse.Query(Message);
+  try {
+    const message = await query.get(messageId);
+    message.set(_unread, false);
+    await message.save();
+  } catch (error) {
+    console.error("Error while marking message as read:", error);
+    throw error;
+  }
 }
 
 export async function getProfileImage(userId) {
   const userQuery = new Parse.Query(Parse.User);
-  userQuery.equalTo('objectId', userId);
+  userQuery.equalTo("objectId", userId);
   let result = await userQuery.first();
   return result.get(_profile_image);
 }
 
-export async function setProfileImage(user, file){
-  if(!user) return 'Please provide a user';
-  if(!file) return 'Please provide a file';
+export async function setProfileImage(user, file) {
+  if (!user) return "Please provide a user";
+  if (!file) return "Please provide a file";
   try {
-    const fileName = user.id+'-profile.jpg';
+    const fileName = user.id + "-profile.jpg";
     const parseFile = new Parse.File(fileName, file);
     await parseFile.save();
-    await user.save('profile_image', parseFile);
-    return 'Profile image updated successfully!';
+    await user.save("profile_image", parseFile);
+    return "Profile image updated successfully!";
+  } catch (error) {
+    return "Error updating profile image:", error;
   }
-  catch (error) {
-    return ('Error updating profile image:', error);
+}
+
+export async function setNewPassword(user, password) {
+  if (!user) return "Please provide a user";
+  if (!password) return "Please provide a password";
+  try {
+    await user.save("password", password);
+    return "Password updated successfully!";
+  } catch (error) {
+    return "Error updating password:", error;
   }
 }
 
 export async function collectProfileData(userId) {
   const userQuery = new Parse.Query(Parse.User);
-    userQuery.equalTo('objectId', userId);
-    let query = await userQuery.first();
+  userQuery.equalTo("objectId", userId);
+  let query = await userQuery.first();
   let profile = {
-    profileImage: query.get('profile_image')?._url,
-    dockNr: query.get('dock'),
-    firstName: query.get('first_name'),
-    lastName: query.get('last_name'),
-    phoneNr: query.get('phone_no'),
-    email: query.get('email'),
-  }
+    profileImage: query.get("profile_image")?._url,
+    dockNr: query.get("dock"),
+    firstName: query.get("first_name"),
+    lastName: query.get("last_name"),
+    phoneNr: query.get("phone_no"),
+    email: query.get("email"),
+  };
   return profile;
 }
