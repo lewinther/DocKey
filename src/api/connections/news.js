5859f4d9edace2cf1028ade64c31082b7b363f69
lewinther <125894035+lewinther@users.noreply.github.com>
2024-03-10 20:30:50 +0100

    connected news to supabase

--- src/api/connections/news.js
+++ src/api/connections/news.js
@@ -1,24 +1,24 @@
+import {supabase} from "../supabase/supabaseClient";
 export async function fetchNewsArticles() {
-    return Promise.resolve([{
-        id: 0,
-        title:'Lorem ipsum is cool',
-        date: '2024-02-20T15:44:00+00z',
-        image: 'http://tinyurl.com/m8eeusct', 
-        text: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
-    },
-    {
-        
-        id: 1,
-        title:'Lorem ipsum is cool',
-        date: '2024-02-20T15:44:00+00z',
-        image: 'http://tinyurl.com/m8eeusct', 
-        text: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
-    },
-    {
-        id: 2,
-        title:'Lorem ipsum is cool',
-        date: '2024-02-20T15:44:00+00z',
-        image: 'http://tinyurl.com/m8eeusct', 
-        text: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
-    }]);
+    const {data, error} = await supabase
+    .from('news_articles')
+    .select();
+    if(error){
+        console.log(error.message);
+        throw new error(error);
+    }
+
+    const articles = [];
+    data.map(x => {
+        articles.push(
+            {
+                id: x.id,
+                title: x.title,
+                date: x.created_at,
+                image: x.image, 
+                text: x.text
+            });
+        }
+    );
+    return articles;
 }
